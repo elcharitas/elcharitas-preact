0642b50507527cff3f8186571f4bfcf9283228a2
elcharitas <jonathanirhodia@gmail.com>
2022-03-19 10:46:11 +0100

    chore: fix banner

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,65 +1,68 @@
 import { h } from "preact";
-import { chakra, Flex, Badge, SimpleGrid } from "@chakra-ui/react";
+import { chakra, Flex, Badge, SimpleGrid, Box } from "@chakra-ui/react";
 import Banner from "../../assets/header.jpg";
 
 const Home = () => {
     return (
-        <SimpleGrid
-            columns={{ base: 1, md: 2 }}
-            spacing={0}
+        <Box
             h="90vh"
-            bgSize="contain"
-            bgPos="center"
+            top={-100}
+            bgSize="cover"
             bgRepeat="no-repeat"
             bg={`url(${Banner})`}
-            _after={{
-                bg: "brand.500",
-                opacity: 0.25,
-                pos: "absolute",
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                zIndex: -1,
-                content: '" "',
-            }}
         >
-            <Flex
-                direction="column"
-                alignItems="start"
-                justifyContent="center"
-                px={{ base: 4, lg: 20 }}
-                py={24}
+            <SimpleGrid
+                columns={{ base: 1, md: 2 }}
+                spacing={0}
+                _after={{
+                    bg: "brand.500",
+                    opacity: 0.25,
+                    pos: "absolute",
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    zIndex: -1,
+                    content: '" "',
+                }}
             >
-                <Badge
-                    color="white"
-                    py={1}
-                    variant="solid"
-                    colorScheme="brand"
-                    rounded="full"
+                <Flex
+                    direction="column"
+                    alignItems="start"
+                    justifyContent="center"
+                    px={{ base: 4, lg: 20 }}
+                    py={24}
                 >
-                    Jonathan Irhodia
-                </Badge>
-                <chakra.h1
-                    mb={6}
-                    fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
-                    fontWeight="bold"
-                    color="white"
-                    lineHeight="shorter"
-                >
-                    Blockchain Engineer
-                </chakra.h1>
-                <chakra.p
-                    pr={{ base: 0, lg: 16 }}
-                    mb={4}
-                    fontSize="sm"
-                    color={"gray.100"}
-                    letterSpacing="wider"
-                >
-                    A design centric software genius
-                </chakra.p>
-            </Flex>
-        </SimpleGrid>
+                    <Badge
+                        color="white"
+                        py={1}
+                        variant="solid"
+                        colorScheme="brand"
+                        rounded="full"
+                    >
+                        Jonathan Irhodia
+                    </Badge>
+                    <chakra.h1
+                        mb={6}
+                        fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
+                        fontWeight="bold"
+                        color="white"
+                        lineHeight="shorter"
+                    >
+                        Blockchain Engineer
+                    </chakra.h1>
+                    <chakra.p
+                        pr={{ base: 0, lg: 16 }}
+                        mb={4}
+                        fontSize="sm"
+                        color={"gray.100"}
+                        letterSpacing="wider"
+                    >
+                        A design centric software genius
+                    </chakra.p>
+                </Flex>
+            </SimpleGrid>
+        </Box>
     );
 };
 
