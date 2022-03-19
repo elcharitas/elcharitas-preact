5a12ae4b810ea3d2218d047a2544c5019132b49d
elcharitas <jonathanirhodia@gmail.com>
2022-03-19 11:49:07 +0100

    feat: add tsparticles

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,14 @@
 import { h } from "preact";
 import { chakra, Flex, Badge, SimpleGrid, Box } from "@chakra-ui/react";
 import Banner from "../../assets/header.jpg";
+import Particles from "react-tsparticles";
 
 const Home = () => {
     return (
         <>
             <Box
+                as={Particles}
+                id="particles"
                 pos="absolute"
                 h="100vh"
                 top={0}
@@ -16,6 +19,83 @@ const Home = () => {
                 bgRepeat="no-repeat"
                 bg={`url(${Banner}) center`}
                 zIndex={-100}
+                options={{
+                    background: {
+                        color: {
+                            value: "transparent",
+                        },
+                    },
+                    fpsLimit: 60,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 400,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40,
+                            },
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outMode: "bounce",
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.3,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            random: true,
+                            value: 2,
+                        },
+                    },
+                    detectRetina: true,
+                }}
             />
             <SimpleGrid
                 columns={{ base: 1, md: 2 }}
