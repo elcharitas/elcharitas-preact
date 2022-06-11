a583f46a5745f4c12b43214511e898b8e4fa9064
elcharitas <jonathanirhodia@gmail.com>
2022-06-11 06:46:09 +0100

    chore: create layout to prevent rerenders

---
+++ src/components/PageLayout.js
@@ -0,0 +1,102 @@
+import { h } from "preact";
+import {
+  chakra,
+  Flex,
+  Badge,
+  SimpleGrid,
+  Box,
+  ScaleFade,
+} from "@chakra-ui/react";
+import Banner from "../../assets/header.jpg";
+import Particles from "react-tsparticles";
+
+const PageLayout = ({ children }) => (
+  <>
+    <Box
+      id="particles"
+      key="particles"
+      as={Particles}
+      zIndex={-100}
+      options={{
+        background: {
+          color: {
+            value: "transparent",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.3,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 2,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+    {children}
+  </>
+);
+
+export default PageLayout;
